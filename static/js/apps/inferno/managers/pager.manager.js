714d270dcbbd0ca267a7133083b98d279cde2eca
Harris Baptiste <harris.baptiste@gmail.com>
2015-06-25 08:52:07 +0200

    fixing pager

--- static/js/apps/inferno/managers/pager.manager.js
+++ static/js/apps/inferno/managers/pager.manager.js
@@ -7,34 +7,57 @@ define(["Kimo/core", "bi.components/cantopager/main"], function(Kimo) {
             pageContainer: "#canto-pager",
             selectedCls: "selected"
         },
-        
         cantoMapping = {
             1: "I",
-            5 : "V",
+            5: "V",
             10: "X"
         },
-       
-        deciToRoman = function (deciNum) {
-            
-        },
-                
-        pagerInstance = null,
-        
-        selectCanto = function(no) {
+        toRoman = function(number) {
+
+            var ten = Math.floor(number / 10);
+            var units = number % 10;
+            var roman = "";
+            for (var i = 0; i < ten; i++) {
+                roman = roman + "X";
+            }
+            if (units >= 5 && units < 9) {
+                var roman = roman + "V";
+                var rem = units - 5;
+                for (var i = 0; i < rem; i++) {
+                    roman = roman + "I";
+                }
+            }
+            else if (units == 4) {
+                roman = roman + "IV";
+
+            }
+
+            else if (units == 9) {
+                roman = roman + "IX";
+            }
+            else {
+                for (var i = 0; i < units; i++) {
+                    roman = roman + "I";
+                }
+            }
+
+            return roman;
+        }
+        ,
+                pagerInstance = null,
+                selectCanto = function(no) {
             pagerInstance.selectCanto(no);
         },
-
-        showPager = function(container) {
+                showPager = function(container) {
             pagerInstance.render(container);
         },
-
-        configure = function (settings) {
+                configure = function(settings) {
             try {
                 pagerInstance = Kimo.createEntityView("CantoPager", {
                     root: settings.root,
                     settings: {
                         itemRenderer: function(page) {
-                            return Kimo.jQuery("<div/>").text(page).addClass("canto-link");
+                            return Kimo.jQuery("<div/>").text("Chan " + toRoman(page)).addClass("canto-link");
                         }
                     }
                 });
@@ -44,8 +67,7 @@ define(["Kimo/core", "bi.components/cantopager/main"], function(Kimo) {
                 console.log(e);
             }
         },
-
-        getApi = function () {
+                getApi = function() {
             return {
                 selectCanto: selectCanto,
                 showCantoPager: showPager,
@@ -54,7 +76,7 @@ define(["Kimo/core", "bi.components/cantopager/main"], function(Kimo) {
                 }
             }
         }
-        return { configure: configure };
+        return {configure: configure};
     }());
     return PagerManager;
 
