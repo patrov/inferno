54c4869ee4bbe14c16ab6434d459378b24f645fd
Harris <harris.baptiste@gmail.com>
2015-12-01 19:08:03 +0100

    serving static

--- app/main/main_app.py
+++ app/main/main_app.py
@@ -1,13 +1,16 @@
 from flask import Blueprint, request, render_template, g, session, redirect, url_for
-
 from pprint import pprint
     
 main_app = Blueprint("main", __name__, template_folder = "/templates")
-
+from app import app
 @main_app.route("/app")
 def index():
     return render_template('inferno.html')
-    
+ 
+@main_app.route('/static/js/<path:filename>')
+def download_file(filename):
+     return app.send_static_file("js/" + filename)
+ 
 
 @main_app.errorhandler(404)
 def page_not_found(error):
