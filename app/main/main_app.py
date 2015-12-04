7768675a47bf90a86c8f99181ec3615d65bf2766
Harris <harris.baptiste@gmail.com>
2015-12-04 13:18:11 +0100

    fixing app

--- app/main/main_app.py
+++ app/main/main_app.py
@@ -8,15 +8,6 @@ from app import app
 def index():
     return render_template('inferno.html')
  
-@main_app.route('/static/js/<path:filename>')
-def download_file(filename):
-     
-     path = os.path.join(os.path.sep, app.static_folder, 'js', filename)
-     print os.path.isfile(path)
-     print path
-     return path
-     #return app.send_static_file(path)
- 
 
 @main_app.errorhandler(404)
 def page_not_found(error):
